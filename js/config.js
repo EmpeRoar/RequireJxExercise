a3f376a1e10c0310d4183aad5edc04965d124296
juliusbacosa <juliusbacosa@gmail.com>
2018-04-11 22:42:52 +0800

    Init

---
+++ js/config.js
@@ -0,0 +1,10 @@
+requirejs.config({
+    baseUrl: 'node_modules',
+    paths: {
+        jquery: [
+            'https://code.jquery.com/jquery-3.3.1.min',
+            'jquery/dist/jquery.min'
+        ],
+        methods: 'customJs/methods'        
+    }
+});
